13e9150bb057e5cc7946ec6c8099b74444300a5d
Pavel Kostikov <pkostikov@yandex-team.ru>
2015-02-17 18:50:55 +0300

    more fixes

--- samples/python/flask-swagger/main.py
+++ samples/python/flask-swagger/main.py
@@ -6,40 +6,45 @@ import opster
 app = flask.Flask(__name__)
 
 
-def send_json(path_to_file):
-    with open(path_to_file) as f:
-        content = f.read()
-    resp = flask.make_response(content)
+def send_json_data(data):
+    print flask.request.headers
+    resp = flask.make_response(data)
     resp.mimetype = "application/json"
-    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Origin'] = 'http://hadar.backa.dev.yandex.net:18005'
     resp.headers['Access-Control-Allow-Methods'] = 'GET, POST, DELETE, PUT, PATCH, OPTIONS'
     resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, api_key, Authorization'
     return resp
 
 
+def send_json_from_file(path_to_file):
+    with open(path_to_file) as f:
+        content = f.read()
+    return send_json_data(content)
+
+
 @app.route("/swagger1.2.header.json")
 def get_header_api_1_2():
-    return send_json('swagger1.2.header.json')
+    return send_json_from_file('swagger1.2.header.json')
 
 
 @app.route("/swagger1.2.header.json/")
 def get_header_api_1_2_():
-    return send_json('swagger1.2.header.json')
+    return send_json_from_file('swagger1.2.header.json')
 
 
 @app.route("/swagger1.2.json")
 def get_api_1_2():
-    return send_json('swagger1.2.json')
+    return send_json_from_file('swagger1.2.json')
 
 
 @app.route("/swagger2.0.json")
 def get_api_2_0():
-    return send_json('swagger2.0.json')
+    return send_json_from_file('swagger2.0.json')
 
 
 @app.route("/handle")
 def get_handle():
-    return '{"hello": "world"}'
+    return send_json_data('{"hello": "world"}')
 
 
 @opster.command()
