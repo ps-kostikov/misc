523dd0fec50355acb84fb8638cf865d2589ac165
Pavel Kostikov <pkostikov@yandex-team.ru>
2015-02-17 13:02:26 +0300

    support 1.2 and 2.0 versions

--- samples/python/flask-swagger/main.py
+++ samples/python/flask-swagger/main.py
@@ -11,16 +11,35 @@ def send_json(path_to_file):
         content = f.read()
     resp = flask.make_response(content)
     resp.mimetype = "application/json"
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET, POST, DELETE, PUT, PATCH, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, api_key, Authorization'
     return resp
 
 
-@app.route("/swagger.json")
-def get_api():
-    return send_json('swagger.json')
+@app.route("/swagger1.2.header.json")
+def get_header_api_1_2():
+    return send_json('swagger1.2.header.json')
+
+
+@app.route("/swagger1.2.header.json/")
+def get_header_api_1_2_():
+    return send_json('swagger1.2.header.json')
+
+
+@app.route("/swagger1.2.json")
+def get_api_1_2():
+    return send_json('swagger1.2.json')
+
+
+@app.route("/swagger2.0.json")
+def get_api_2_0():
+    return send_json('swagger2.0.json')
+
 
 @app.route("/handle")
 def get_handle():
-    return 'hello'
+    return '{"hello": "world"}'
 
 
 @opster.command()
