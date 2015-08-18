f4f7ee5bbae7e5b72cd0b8d8976bd2a72be19d38
Pavel Kostikov <pkostikov@yandex-team.ru>
2015-08-18 17:13:28 +0300

    xml gen sample

---
+++ samples/cpp/yandex-maps-xml3/gen/sample.cpp
@@ -0,0 +1,28 @@
+#include <yandex/maps/xml3/xml.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+namespace xml3 = maps::xml3;
+
+int main()
+{
+    std::vector<std::string> versions{
+        "1.0.0",
+        "2.1.0",
+        "3.0.2"
+    };
+
+    xml3::Doc doc(xml3::Doc::create("versions"));
+    xml3::Node root = doc.root();
+    for (const auto& version: versions) {
+        root.addChild("version", version);
+    }
+
+    std::string xmlStr;
+    doc.save(xmlStr);
+    std::cout << xmlStr << std::endl;
+    return 0;
+}
