7cf0fd253baeda936868f35703fc6569663f46ee
Pavel Kostikov <pkostikov@yandex-team.ru>
2015-11-16 14:40:16 +0300

    enum with numbers sample

--- samples/cpp/language/enum/sample.cpp
+++ samples/cpp/language/enum/sample.cpp
@@ -6,11 +6,22 @@ enum Type {
     Type3,
 };
 
+enum Enum {
+    Enum1 = 1,
+    Enum2 = 2,
+    Enum3 = 3,
+};
+
 int main()
 {
     std::cout << "hello world" << std::endl;
     std::cout << "Type1: " << Type::Type1 << std::endl;
     std::cout << "Type2: " << Type::Type2 << std::endl;
     std::cout << "Type3: " << Type::Type3 << std::endl;
+
+    std::cout << "Enum1: " << Enum::Enum1 << std::endl;
+    std::cout << "Enum2: " << Enum::Enum2 << std::endl;
+    std::cout << "Enum3: " << Enum::Enum3 << std::endl;
+
     return 0;
 }
