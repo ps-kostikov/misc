c8178a96d2f49a7a454c508adebfc0a2dd7689c7
Pavel Kostikov <pkostikov@yandex-team.ru>
2017-01-12 14:19:59 +0300

    some scripts

---
+++ samples/telegram-bot/chat_wars/base_commands/yellow.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TG_CLI_PATH=~/tmp/tg
+
+$TG_CLI_PATH/bin/telegram-cli -k $TG_CLI_PATH/tg-server.pub -W -e "msg Chat_Wars 🇻🇦"
---
+++ samples/telegram-bot/chat_wars/scenarios/attack_white.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BC=~/Code/chat_wars_bot/base_commands
+
+$BC/attack.sh
+sleep 10
+$BC/white.sh
