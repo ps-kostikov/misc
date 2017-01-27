5943d348ca13c8e230759ef76e19c03c0b0a2838
Pavel Kostikov <pkostikov@yandex-team.ru>
2017-01-27 16:38:09 +0300

    dummy approach complete

--- samples/cpp/wikimap/pins/yacare.cpp
+++ samples/cpp/wikimap/pins/yacare.cpp
@@ -2,6 +2,7 @@
 #include <yandex/maps/pgpool3/pgpool3.h>
 #include <yandex/maps/json/builder.h>
 #include <yandex/maps/json/value.h>
+#include <yandex/maps/tileutils5/tile.h>
 
 #include <boost/optional.hpp>
 
@@ -11,6 +12,7 @@
 
 namespace mpgp = maps::pgpool3;
 namespace mj = maps::json;
+namespace mtu = maps::tileutils5;
 
 std::unique_ptr<mpgp::Pool> g_poolPtr;
 
@@ -52,6 +54,20 @@ getOptionalParam(const yacare::Request& r, const std::string& name)
     }
 }
 
+std::string mercatorWkt(uint64_t x, uint64_t y, uint64_t z)
+{
+    mtu::Tile tile(x, y, z);
+    auto mBox = tile.mercatorBox();
+    std::stringstream ss;
+    ss << "POLYGON ((";
+    ss << mBox.lt().x() << " " << mBox.lt().y() << ", ";
+    ss << mBox.rb().x() << " " << mBox.lt().y() << ", ";
+    ss << mBox.rb().x() << " " << mBox.rb().y() << ", ";
+    ss << mBox.lt().x() << " " << mBox.rb().y() << ", ";
+    ss << mBox.lt().x() << " " << mBox.lt().y();
+    ss << "))";
+    return ss.str();
+}
 
 // yacare::ThreadPool heavyPool(/* name = */ "heavy", /* threads  = */ 32, /* backlog = */ 16);
 // YCR_RESPOND_TO("sample:/signals-renderer", YCR_IN_POOL(heavyPool))
@@ -66,19 +82,18 @@ YCR_RESPOND_TO("sample:/tiles")
     }
     auto callbackOpt = getOptionalParam<std::string>(request, "callback");
 
-    // auto connection = g_poolPtr->getMasterConnection();
-    // auto transaction = mpgp::makeWriteableTransaction(std::move(connection));
-    // std::stringstream query;
-    // // query << "select mds_key from signals.signals_tiles where style = 'point'"
-    // query << "select mds_key from signals_tiles where style = 'point'"
-    //     << " and x = " << *xOpt
-    //     << " and y = " << *yOpt
-    //     << " and z = " << *zOpt
-    //     << ";";
-    // auto postgresQueryBegin = std::chrono::system_clock::now();
-    // auto result = transaction->exec(query.str());
-    // auto postgresQueryEnd = std::chrono::system_clock::now();
-    // std::chrono::duration<double> postgresQueryDuration = postgresQueryEnd - postgresQueryBegin;
+    auto mWkt = mercatorWkt(*xOpt, *yOpt, *zOpt);
+    // std::cout << mWkt << std::endl;
+
+    auto connection = g_poolPtr->getMasterConnection();
+    auto transaction = mpgp::makeWriteableTransaction(std::move(connection));
+    std::stringstream query;
+    query << "select id, comment, st_x(position) as x, st_y(position) as y from pkostikov.pins_all ";
+    query << "where st_within(position, st_transform(st_setsrid(st_geomfromtext('" << mWkt<< "'), 3395), 4326))";
+    auto postgresQueryBegin = std::chrono::system_clock::now();
+    auto result = transaction->exec(query.str());
+    auto postgresQueryEnd = std::chrono::system_clock::now();
+    std::chrono::duration<double> postgresQueryDuration = postgresQueryEnd - postgresQueryBegin;
 
     // if (result.empty()) {
     //     std::cout << "postgres time " << postgresQueryDuration.count() << "sec" << std::endl;
@@ -87,61 +102,38 @@ YCR_RESPOND_TO("sample:/tiles")
 
 // GET /signals-renderer?x=74533&y=45031&z=17
 
-    // auto imageBegin = std::chrono::system_clock::now();
-    // auto imageEnd = std::chrono::system_clock::now();
-    // std::chrono::duration<double> imageDuration = imageEnd - imageBegin;
-
-    // std::cout << "postgres time " << postgresQueryDuration.count() << "sec; "
-    //     << "mds time " << mdsRequestDuration.count() << "sec; "
-    //     << "image time " << imageDuration.count() << "sec" << std::endl;
-
-
-    // res = {
-    //   "type": "FeatureCollection",
-    //   "features": [
-    //     {
-    //       "type": "Feature",
-    //       "id": x + y * 2**20 + z * 2**40,
-    //       "geometry": {
-    //         "type": "Point",
-    //         "coordinates": [point.y, point.x]
-    //       },
-    //       "properties": {
-    //         "balloonContent": "content",
-    //         "clusterCaption": "label 1",
-    //         "hintContent": "Hint text"
-    //       }
-    //     }
-    //   ]
-    // }
+    std::cout << "postgres time " << postgresQueryDuration.count() << "sec" << std::endl;
+    
 
     // double x = 37.61718;
 
 
     mj::Builder builder;
-    builder << [](mj::ObjectBuilder b) {
+    builder << [&result](mj::ObjectBuilder b) {
         b["type"] = "FeatureCollection";
-        b["features"] = [](mj::ArrayBuilder b) {
-            b << [](mj::ObjectBuilder b) {
-                b["type"] = "Feature";
-                b["id"] = 1;
-                b["geometry"] = [](mj::ObjectBuilder b) {
-                    b["type"] = "Point";
-                    b["coordinates"] = [](mj::ArrayBuilder b) {
-                        b << 55.75744;
-                        b << 37.61718;
+        b["features"] = [&result](mj::ArrayBuilder b) {
+            for (const auto& row: result) {
+                b << [&row](mj::ObjectBuilder b) {
+                    b["type"] = "Feature";
+                    b["id"] = row["id"].as<uint64_t>();
+                    b["geometry"] = [&row](mj::ObjectBuilder b) {
+                        b["type"] = "Point";
+                        b["coordinates"] = [&row](mj::ArrayBuilder b) {
+                            b << row["y"].as<double>();
+                            b << row["x"].as<double>();
+                        };
+                    };
+                    b["properties"] = [&row](mj::ObjectBuilder b) {
+                        b["balloonContent"] = row["comment"].as<std::string>();
+                        b["clusterCaption"] = "lable 1";
+                        b["hintContent"] = row["comment"].as<std::string>();
                     };
                 };
-                b["properties"] = [](mj::ObjectBuilder b) {
-                    b["balloonContent"] = "content";
-                    b["clusterCaption"] = "lable 1";
-                    b["hintContent"] = "Hint text";
-                };
-            };
+            }
         };
     };
 
-    response["Content-Type"] = "application/json";
+    response["Content-Type"] = "application/json; charset=utf-8";
     if (callbackOpt) {
         response << *callbackOpt << "(" << builder.str() << ")";
     } else {
