4597e6ec524461901adc05e99e1c110cd9473e60
Pavel Kostikov <ps-kostikov@yandex.ru>
2019-03-19 17:26:27 +0300

    unicorn + flask sample app

---
+++ samples/python/unicorn-flask/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, Blueprint
+import time
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
+
+
+test_bp = Blueprint('test', 'bp_name')
+
+@test_bp.record_once
+def init_blueprint(state):
+    for i in range(100000000): j=i
+    logging.info('bp record once')
+
+@test_bp.route('/test')
+def test():
+    return 'Hello, World!'
+
+
+def create_app():
+    for i in range(100000000): j=i
+    logging.info("long operation before app created")
+    return Flask('app_name')
+
+app = create_app()
+app.register_blueprint(test_bp)
